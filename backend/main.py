a4dae6c790f8674a4af102f6663020f116354a49
naufalmaa <naufalmadzkia@gmail.com>
2026-08-14 17:05:50 +0700

    Hands On Sesi 3: Migrasi dari Console Python ke FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,124 +1,74 @@
 from services.trip_services import calculate_daily_budget, get_trip_category, get_transport_recommendation, get_recommended_places, get_travel_season
 from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-"""
-KelanaAI - Trip Summary Generator
-Sesi 1: Aplikasi Konsol Berbasis Python
-"""
+# """
+# KelanaAI - Trip Summary Generator
+# Sesi 3: Basis API Dengan RESTful & FastAPI
+# """
 
-def print_trip_summary(
-    total_destination: int,
-    destination: List[str],
-    country: str,
-    days: int,
-    budget: float,
-    currency: str,
+class TripRequest (BaseModel):
+
+    destination: List[str]
+    country: str
+    days: int
+    budget: float
+    currency: str
+    travel_style: str
     travel_month: str
-) -> None:
-    """
-    Fungsi untuk membungkus logika pencetakan ringkasan rencana perjalanan.
-    Menggunakan f-strings agar tampilan output rapi, terstruktur, dan mudah dibaca.
-    """
-    # Format budget agar rapi (menampilkan angka bulat jika tidak ada nilai desimal)
-    formatted_budget = f"{int(budget) if budget.is_integer() else budget} {currency}"
 
-    # Get Trip Category
-    daily_budget = int(calculate_daily_budget(budget, days))
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message":"Welcome to KelanaAI"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status":"OK"
+    }
+
+@app.get("/api/v1/trip_categories")
+def trip_categories():
+    return [
+        "Backpacker",
+        "Standard",
+        "Luxury"
+    ]
+
+@app.post("/api/v1/trips")
+def create_trip(request: TripRequest):
+    destination = request.destination
+    country = request.country
+    days = request.days
+    budget = request.budget
+    currency = request.currency
+    travel_month = request.travel_month
+    travel_style = request.travel_style
+    daily_budget = calculate_daily_budget(budget, days)
     trip_category = get_trip_category(budget)
-
-    # Get Transport Recommendation
-    recommended_transportation = get_transport_recommendation(trip_category)
-
-    # Get Place Recommendation
-    recommended_places = {}
-    for index in range (len(destination)):
-        recommended_places[destination[index]] = get_recommended_places(destination[index])
-
-    print_recommended_places = '\n'.join([f"\n{key}: \n{', '.join(value)}" for key, value in recommended_places.items()])
-
+    transport = get_transport_recommendation(trip_category)
     travel_season = get_travel_season(travel_month)
-
-    print("\n========================")
-    print("KelanaAI")
-    print("========================")
-    print(f"Total Destination           : {total_destination}")
-    print(f"Destination                 : {', '.join(destination)}")
-    print(f"Country                     : {country}")
-    print(f"Days                        : {days}")
-    print(f"Budget                      : {formatted_budget}")
-    print(f"Currency                    : {currency}")
-    print(f"Travel Month                : {travel_month}")
-    print(f"Travel Season               : {travel_season}")
-    print(f"Daily Budget                : {daily_budget}")
-    print(f"Trip Category               : {trip_category}")
-    print(f"Recommended Transportation  : {recommended_transportation}")
-    print(f"Recommended Places to Visit : {print_recommended_places}\n\n")
-    print("========================\n")
-
-
-def get_user_inputs():
-    """
-    Meminta input interaktif dari pengguna dengan penanganan konversi tipe data.
-    """
-    print("========================================")
-    print("   Selamat Datang di KelanaAI Generator  ")
-    print("========================================\n")
-
-    total_destination = int(input("Masukkan Total Destinasi : ").strip())
     
-    index = 0
-    destination = []
+    places_list = []
+    for i in destination:
+        places_list.append({"destination": i, "places": get_recommended_places(i)})
     
-    while index < total_destination:
-        destination.append(input(f"Masukkan Destinasi {index+1} : ").strip())
-        index += 1
-
-    country = input("Masukkan Negara      : ").strip()
-
-    # Input & konversi tipe data int()
-    while True:
-        try:
-            days = int(input("Masukkan Jumlah Hari : "))
-            if days <= 0:
-                print("⚠️  Jumlah hari harus berupa angka positif (> 0). Silakan coba lagi.")
-                continue
-            break
-        except ValueError:
-            print("⚠️  Input tidak valid! Harap masukkan angka bulat (integer).")
-
-    # Input & konversi tipe data float()
-    while True:
-        try:
-            budget = float(input("Masukkan Budget      : "))
-            if budget < 0:
-                print("⚠️  Budget tidak boleh negatif. Silakan coba lagi.")
-                continue
-            break
-        except ValueError:
-            print("⚠️  Input tidak valid! Harap masukkan angka desimal/bulat (float).")
-
-    currency = input("Masukkan Mata Uang   : ").strip().upper()
-    travel_month = input("Masukkan Bulan Travel: ").strip().lower()
-
-    return total_destination, destination, country, days, budget, currency, travel_month
-
-
-
-def main():
-    """
-    Fungsi utama untuk menjalankan program KelanaAI.
-    """
-    total_destination, destination, country, days, budget, currency, travel_month = get_user_inputs()
-    print_trip_summary(
-        total_destination=total_destination,
-        destination=destination,
-        country=country,
-        days=days,
-        budget=budget,
-        currency=currency,
-        travel_month=travel_month
-    )
-
-
-if __name__ == "__main__":
-    main()
+    return {
+        "destination": destination,
+        "country": country,
+        "days": days,
+        "budget": budget,
+        "currency": currency,
+        "travel_month": travel_month,
+        "travel_style": travel_style,
+        "travel_season": travel_season,
+        "daily_budget": daily_budget,
+        "trip_category": trip_category,
+        "recommended_transportation": transport,
+        "recommended_places": places_list,
+    }
